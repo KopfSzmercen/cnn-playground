323b744b2eceac761cb55bcb4364cdac88fe945c
gitName <a@b.c>
2025-12-04 12:13:13 +0100

    resnet augmentation

--- main.py
+++ main.py
@@ -16,8 +16,17 @@ BATCH_SIZE = 32
 
 model = models.resnet18()
 
-data_transform = transforms.Compose([
-    transforms.ToTensor()
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding=4),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
+])
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
 ])
 
 model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
@@ -52,8 +61,8 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-cifar_train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=data_transform)
-cifar_test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=data_transform)
+cifar_train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
+cifar_test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
 
 percent = float(args.percent)
 if percent > 1:
