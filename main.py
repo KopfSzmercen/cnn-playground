58fcd36f34e4da2f779eb204c93a99182856c6a5
gitName <a@b.c>
2026-02-22 13:58:20 +0100

    lr 0.005

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ view_random_N_dataloader_images(
 )
 
 loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.005)
 
 EPOCHS = int(args.epochs)
 
